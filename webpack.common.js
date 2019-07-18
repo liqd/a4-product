db4089a34863eac1d34f2d5237805b60f9839ed1
Robert Mader <r.mader@liqd.net>
2019-07-18 15:35:32 +0200

    webpack: Automatically generate 'vendor' chunk
    Webpack 4 brought some handy new features, one of them being chunk
    handling. It allows us to now easily split all vendor code automatically
    into a 'vendor' chunk and also allows us to further differentiate between
    packages, so we can e.g. create one chunk for all leaflet related stuff.

--- webpack.common.js
+++ webpack.common.js
@@ -13,52 +13,53 @@ module.exports = {
     platform: [
       './liqd_product/assets/scss/platform.scss'
     ],
-    vendor: [
+    // product specific hack to work around adhocracy4/platform split
+    common_unincluded: [
       'classnames',
+      'immutability-helper',
+      'js-cookie',
+      'react',
+      'react-dom',
+      'react-flip-move',
+      'slick-carousel/slick/slick.min.js',
+      'typeface-libre-franklin',
       '@fortawesome/fontawesome-free/scss/fontawesome.scss',
       '@fortawesome/fontawesome-free/scss/brands.scss',
       '@fortawesome/fontawesome-free/scss/regular.scss',
       '@fortawesome/fontawesome-free/scss/solid.scss',
-      'js-cookie',
-      'react',
-      'immutability-helper',
-      'react-dom',
-      'react-flip-move',
-      'typeface-libre-franklin',
-      'slick-carousel/slick/slick.min.js',
+      'datepicker/css/datepicker.min.css',
+      'leaflet/dist/leaflet.css',
+      'leaflet-draw/dist/leaflet.draw.css',
+      'leaflet.markercluster/dist/MarkerCluster.css',
+      'mapbox-gl/dist/mapbox-gl.css',
       'slick-carousel/slick/slick.css'
     ],
     a4maps_display_point: [
-      'leaflet/dist/leaflet.css',
-      'mapbox-gl/dist/mapbox-gl.css',
       'adhocracy4/adhocracy4/maps/static/a4maps/a4maps_display_point.js'
     ],
     a4maps_display_points: [
-      'leaflet/dist/leaflet.css',
-      'mapbox-gl/dist/mapbox-gl.css',
-      'leaflet.markercluster/dist/MarkerCluster.css',
       'adhocracy4/adhocracy4/maps/static/a4maps/a4maps_display_points.js'
     ],
     a4maps_choose_point: [
-      'leaflet/dist/leaflet.css',
-      'mapbox-gl/dist/mapbox-gl.css',
       'adhocracy4/adhocracy4/maps/static/a4maps/a4maps_choose_point.js'
     ],
     a4maps_choose_polygon: [
-      'leaflet/dist/leaflet.css',
-      'mapbox-gl/dist/mapbox-gl.css',
-      'leaflet-draw/dist/leaflet.draw.css',
       './apps/maps/assets/map_choose_polygon_with_preset.js'
     ],
     datepicker: [
-      './liqd_product/assets/js/init-picker.js',
-      'datepicker/css/datepicker.min.css'
+      './liqd_product/assets/js/init-picker.js'
     ],
     embed: [
       './liqd_product/assets/js/embed.js'
     ],
     'popup-close': [
       './liqd_product/assets/js/popup-close.js'
+    ],
+    unload_warning: [
+      './apps/contrib/static/js/unload_warning.js'
+    ],
+    imageUploader: [
+      'adhocracy4/adhocracy4/images/static/a4images/imageUploader.js'
     ]
   },
   output: {
@@ -140,13 +141,9 @@ module.exports = {
       $: 'jquery',
       jQuery: 'jquery'
     }),
-    new webpack.optimize.SplitChunksPlugin({
-      name: 'vendor',
-      filename: 'vendor.js'
-    }),
     new MiniCssExtractPlugin({
       filename: '[name].css',
-      chunkFilename: '[id].css'
+      chunkFilename: '[name].css'
     }),
     new CopyWebpackPlugin([
       {
@@ -155,5 +152,24 @@ module.exports = {
         flatten: true
       }
     ])
-  ]
+  ],
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        leaflet: {
+          test: /[\\/]node_modules[\\/](leaflet|leaflet-draw|leaflet.markercluster|mapbox-gl|mapbox-gl-leaflet)[\\/]/,
+          name: 'leaflet',
+          chunks: 'all',
+          priority: 0
+        },
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor',
+          chunks: 'all',
+          priority: -100,
+          reuseExistingChunk: true
+        }
+      }
+    }
+  }
 }
